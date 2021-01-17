4f99bf7cd8b2c8cd921c7e980e5fbf96cfe02ce8
emanuelmilotic56 <58472080+emanuelmilotic56@users.noreply.github.com>
2021-01-17 15:24:23 +0100

    Add files via upload

---
+++ Zadaci za kraj polugodista-5. Zadatak/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadaci_za_kraj_polugodista_5.Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
